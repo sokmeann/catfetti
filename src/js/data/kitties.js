d0e33a5b0dcb35a699e1dca7eea592b0a9ed3c46
Sokmean Nou <sokmean.nou@gmail.com>
2017-02-07 00:30:22 -0500

    changed the cats to other pngs

--- src/js/data/kitties.js
+++ src/js/data/kitties.js
@@ -1,16 +1,22 @@
-import kittyUL from 'src/img/kitty-ul.png';
-import kittyUM from 'src/img/kitty-um.png';
-import kittyUR from 'src/img/kitty-ur.png';
-import kittyLL from 'src/img/kitty-ll.png';
-import kittyLM from 'src/img/kitty-lm.png';
-import kittyLR from 'src/img/kitty-lr.png';
+// import kittyUL from 'src/img/kitty-ul.png';
+// import kittyUM from 'src/img/kitty-um.png';
+// import kittyUR from 'src/img/kitty-ur.png';
+// import kittyLL from 'src/img/kitty-ll.png';
+// import kittyLM from 'src/img/kitty-lm.png';
+// import kittyLR from 'src/img/kitty-lr.png';
+import rey from 'src/img/Rey.png';
+import brFlag from 'src/img/brazil_small.png';
+import soccer from 'src/img/SoccerBallSmall.png';
+import r2d2 from 'src/img/R2-D2_small.png';
+import light1 from 'src/img/magentalight_small.png';
+import light2 from 'src/img/greenlight_small.png';
+import bb8 from 'src/img/bb8_small.png';
 
 const kittySrcs = [
-  kittyUL, kittyUM, kittyUR,
-  kittyLL, kittyLM, kittyLR
+  brFlag, soccer, rey, r2d2, light1, light2, bb8
 ];
 
-const kittyImages = Array(6).fill(null).map((_, idx) => {
+const kittyImages = Array(7).fill(null).map((_, idx) => {
   const kitty = new Image();
   kitty.src = kittySrcs[idx];
   return kitty;
